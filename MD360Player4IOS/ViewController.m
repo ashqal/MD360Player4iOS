bf80a71fcdeb531f68df8f827c33c67c0abe7caf
asha <ashnb@qq.com>
2016-04-06 13:53:48 +0800

    first commit

--- MD360Player4IOS/ViewController.m
+++ MD360Player4IOS/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "VIMVideoPlayerView.h"
+#import "VIMVideoPlayer.h"
 
-@interface ViewController ()
+@interface ViewController()<VIMVideoPlayerViewDelegate>{
+}
+@property (nonatomic, strong) VIMVideoPlayerView *videoPlayerView;
 
 @end
 
@@ -17,11 +21,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.videoPlayerView = [[VIMVideoPlayerView alloc] initWithFrame:CGRectMake(0, 0, [[UIScreen mainScreen] bounds].size.width, [[UIScreen mainScreen] bounds].size.height)];
+    self.videoPlayerView.translatesAutoresizingMaskIntoConstraints = NO;
+    self.videoPlayerView.delegate = self;
+    
+    [self.videoPlayerView setVideoFillMode:AVLayerVideoGravityResizeAspect];
+    [self.videoPlayerView.player enableTimeUpdates];
+    [self.videoPlayerView.player enableAirplay];
+    
+    [self.view addSubview:self.videoPlayerView];
+    
+    NSString* url = [[NSBundle mainBundle] pathForResource:@"skyrim360" ofType:@"mp4"];
+    NSLog(@"url:%@",url);
+    [self.videoPlayerView.player setURL:[NSURL fileURLWithPath:url]];
+    [self.videoPlayerView.player play];
+    //[self.videoPlayerView.player setURL:@""];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)videoPlayerView:(VIMVideoPlayerView *)videoPlayerView timeDidChange:(CMTime)cmTime{
+    NSLog(@"timeDidChange" );
 }
 
 @end
---
+++ Podfile
@@ -0,0 +1,9 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+# use_frameworks!
+
+target 'MD360Player4IOS' do
+pod 'VIMVideoPlayer', '~> 6.0'
+end
+
